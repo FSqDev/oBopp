3f9c7caf014149fefe3f3bd9a0197cf5cb8f918a
Richard Qin <rqin1@ualberta.ca>
2021-01-16 16:32:45 -0700

    button? button!

--- App/obopp/src/components/Camera.jsx
+++ App/obopp/src/components/Camera.jsx
@@ -4,70 +4,37 @@ import Webcam from "react-webcam";
 
 export default function Camera() {
 
-  const WebcamStreamCapture = () => {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
+   
+  const WebcamCapture = () => {
     const webcamRef = React.useRef(null);
-    const mediaRecorderRef = React.useRef(null);
-    const [capturing, setCapturing] = React.useState(false);
-    const [recordedChunks, setRecordedChunks] = React.useState([]);
-
-    const handleDataAvailable = React.useCallback(
-      ({ data }) => {
-        if (data.size > 0) {
-          setRecordedChunks((prev) => prev.concat(data));
-        }
+   
+    const capture = React.useCallback(
+      () => {
+        const imageSrc = webcamRef.current.getScreenshot();
       },
-      [setRecordedChunks]
+      [webcamRef]
     );
-
-    const handleStartCaptureClick = React.useCallback(() => {
-      setCapturing(true);
-      mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-        mimeType: "video/webm"
-      });
-      mediaRecorderRef.current.addEventListener(
-        "dataavailable",
-        handleDataAvailable
-      );
-      mediaRecorderRef.current.start();
-    }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
-
-    const handleStopCaptureClick = React.useCallback(() => {
-      mediaRecorderRef.current.stop();
-      setCapturing(false);
-    }, [mediaRecorderRef, setCapturing]);
-
-    const handleDownload = React.useCallback(() => {
-      if (recordedChunks.length) {
-        const blob = new Blob(recordedChunks, {
-          type: "video/webm"
-        });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        document.body.appendChild(a);
-        a.style = "display: none";
-        a.href = url;
-        a.download = "react-webcam-stream-capture.webm";
-        a.click();
-        window.URL.revokeObjectURL(url);
-        setRecordedChunks([]);
-      }
-    }, [recordedChunks]);
-
+   
     return (
       <>
-        <Webcam audio={false} ref={webcamRef} />
-        {capturing ? (
-          <Button onClick={handleStopCaptureClick}>Stop Capture</Button>
-        ) : (
-            <Button onClick={handleStartCaptureClick}>Start Capture</Button>
-          )}
-        {recordedChunks.length > 0 && (
-          <Button onClick={handleDownload}>Download</Button>
-        )}
+        <Webcam
+          audio={false}
+          height={720}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width={1280}
+          videoConstraints={videoConstraints}
+        />
+        <Button onClick={capture}>Capture photo</Button>
       </>
     );
   };
 
-  return (WebcamStreamCapture());
+  return (WebcamCapture());
 
 }
