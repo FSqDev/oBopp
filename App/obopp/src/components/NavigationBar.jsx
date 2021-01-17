749f5d032e44e74cfad12ba9c218ee8db2ffecc8
Richard Qin <rqin1@ualberta.ca>
2021-01-16 23:39:42 -0700

    Revert "Revert "i don't remember what happened""
    This reverts commit 1a740d78895e667347e5704db233a67167b7b2be.

--- App/obopp/src/components/NavigationBar.jsx
+++ App/obopp/src/components/NavigationBar.jsx
@@ -5,7 +5,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
@@ -13,7 +12,7 @@ import Badge from '@material-ui/core/Badge';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { mainListItems } from './listItems';
+import ListItems from './ListItems';
 import Camera from "./Camera";
 import Dashboard from "./Dashboard";
 import {
@@ -152,20 +151,15 @@ export default function NavigationBar() {
           }}
           open={open}
         >
-          <div className={classes.toolbarIcon}>
-            <IconButton onClick={handleDrawerClose}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </div>
           <Divider />
-          <List>{mainListItems}</List>
+          <ListItems />
           <Divider />
         </Drawer>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Container className={classes.contents}>
           <Switch>
-            <Route path="/ihatedarian">
+            <Route path="/cameracapture">
               <Camera />
             </Route>
             <Route path="/">
