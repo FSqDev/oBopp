4ed6e8bb0641efd3ea84f3085a74412f702331b4
Richard Qin <rqin1@ualberta.ca>
2021-01-16 17:07:10 -0700

    forgot to save lmao

--- App/obopp/src/components/Gallery.jsx
+++ App/obopp/src/components/Gallery.jsx
@@ -3,6 +3,6 @@ import lion_cat from '../image/lion_cat.jpg'
 
 export default function Gallery() {
     return (
-        <img src={lion_cat}/>
+        <img src={lion_cat} alt="placeholder_image"/>
     );
 }
